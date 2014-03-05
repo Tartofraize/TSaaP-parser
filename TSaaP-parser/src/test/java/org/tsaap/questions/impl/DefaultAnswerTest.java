cf2f8656cbe1bd1d333d7b62f1adbfbbc4c3d864
tartofraize <tartofraize@localhost>
2014-03-05 14:43:10 +0100

    Premier commit. Mise en place du projet. Le code source de TSaaP-notes à été copié dans le dossier src/main. Le dossier src/test contient un premier test unitaire vide. Les rapports sont générés sans erreur (checkstyle et cobertura). Il faut modifier le code copié (partie parser) afin de prendre en compte des questions au format wikiversity.

---
+++ TSaaP-parser/src/test/java/org/tsaap/questions/impl/DefaultAnswerTest.java
@@ -0,0 +1,16 @@
+package org.tsaap.questions.impl;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DefaultAnswerTest {
+
+	@Test
+	public void test() {
+		DefaultAnswer d = new DefaultAnswer();
+		d.getTextValue();
+		assertTrue(true);
+	}
+
+}
