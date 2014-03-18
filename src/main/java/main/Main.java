fe161f8b3a0c3d4f635e20dae24ce360e892c45a
heiva <loic.laiwoa@gmail.com>
2014-03-18 15:38:45 +0100

    #14 recommit suite au pb du premier

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,15 +3,14 @@ package main;
 import gift.GiftQuestionService;
 import gift.GiftReaderException;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
-import questions.implementation.QuestionImpl;
 import questions.interfaces.Question;
-import questions.interfaces.QuestionBlock;
 import questions.interfaces.TextBlock;
 import quizz.implementation.QuizImpl;
 import reponses.interfaces.Answer;
-import reponses.interfaces.AnswerBlock;
 
 
 
@@ -19,26 +18,52 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Stub de la méthode généré automatiquement
+		String quizz;
+		String filePath = "/home/heiva/workspace/TSaaP-parser/ressources/questions";
+		
+		quizz = recuperationQuestion(filePath);
+		
 
 		GiftQuestionService me = new GiftQuestionService();
-		
 		try {
-			QuestionImpl test = (QuestionImpl) me.getQuestionFromGiftText("::Grant's tomb ::Qui repose dans la Grant's tomb à New-York? {=Grant~Personne#C'était vrai pendant 12 ans, mais la dépouille de Grant a été enterrée dans cette tombe en 1897.~Napoléon#Il a été enterré en France.~Churchill#Il a été enterré en Angleterre.~Mère Teresa#Elle a été enterrée en Inde.}");
-			
-			System.out.println(test.getTitle());
-			System.out.println(test.getTextBlockList().get(0).getText());
-			
-			for (AnswerBlock txt : test.getAnswerBlockList()) {
-				for (Answer ans : txt.getAnswerList()) {
-					System.out.println(ans.getTextValue());
+			QuizImpl test = (QuizImpl) me.getQuizFromGiftText(quizz);
+			for (Question question : test.getQuestionList()) {
+				System.out.println(question.getTitle());
+				for (TextBlock txt : question.getTextBlockList()) {
+					System.out.println(txt.getText());
 				}
+				for (Answer txt : question.getAnswerBlockList().get(0).getAnswerList()) {
+					System.out.println(txt.getTextValue());
+				}
+
 			}
 			
 			//me.getQuizFromGiftText("{Question\n|type=\"()\"}\n+ The correct answer.\n- Distractor.\n- Distractor.\n- Distractor.");
 		} catch (GiftReaderException | IOException e) {
 			// TODO Bloc catch généré automatiquement
 			e.printStackTrace();
+		}		
+	}
+
+	private static String recuperationQuestion(String filePath) {
+		
+		String question = new String();
+		
+		try{
+			BufferedReader buff = new BufferedReader(new FileReader(filePath));	
+			
+			try {
+				String line;
+				while ((line = buff.readLine()) != null) {
+					question += line;
+				}
+			} finally {
+				buff.close();
+			}
+		} catch (IOException ioe) {
+			System.out.println("Erreur --" + ioe.toString());
 		}
+		return question;
 	}
 
 }
