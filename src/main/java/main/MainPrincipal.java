8d3eef10976888e32f14dfd86727c998fb05996f
Heiva <loic.laiwoa@gmail.com>
2014-03-20 18:58:21 +0100

    #12 Modification du main principal pour prendre en compte le nouveau parser (Laurine) avec la méthode d'ouverture de fichier J'ai modifie la façon dont on recupere le chemin du fichier ressource.

---
+++ src/main/java/main/MainPrincipal.java
@@ -0,0 +1,81 @@
+package main;
+
+import gift.GiftQuestionService;
+import gift.GiftQuizContentHandler;
+import gift.GiftReaderException;
+import gift.WikiReader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+
+import questions.interfaces.Question;
+import questions.interfaces.TextBlock;
+import quizz.implementation.QuizImpl;
+import reponses.interfaces.Answer;
+
+
+
+public class MainPrincipal {
+
+	public static void main(String[] args) {
+		// TODO Stub de la méthode généré automatiquement
+		String quizz;
+		String filePath = new File("ressources/TestQuizz.txt").getAbsolutePath();
+		System.out.println(filePath);
+		
+		GiftQuizContentHandler handler = new GiftQuizContentHandler();
+        WikiReader quizReader = new WikiReader();
+        quizReader.setQuizContentHandler(handler);
+        quizReader.readFichier(filePath);
+        
+        
+		
+		
+		
+		
+
+//		GiftQuestionService me = new GiftQuestionService();
+//		try {
+//			QuizImpl test = (QuizImpl) me.getQuizFromGiftText(quizz);
+//			for (Question question : test.getQuestionList()) {
+//				System.out.println(question.getTitle());
+//				for (TextBlock txt : question.getTextBlockList()) {
+//					System.out.println(txt.getText());
+//				}
+//				for (Answer txt : question.getAnswerBlockList().get(0).getAnswerList()) {
+//					System.out.println(txt.getTextValue());
+//				}
+//
+//			}
+//			
+//			//me.getQuizFromGiftText("{Question\n|type=\"()\"}\n+ The correct answer.\n- Distractor.\n- Distractor.\n- Distractor.");
+//		} catch (GiftReaderException | IOException e) {
+//			// TODO Bloc catch généré automatiquement
+//			e.printStackTrace();
+//		}		
+	}
+
+	private static String recuperationQuestion(String filePath) {
+		String question = new String();
+		
+		try{
+			BufferedReader buff = new BufferedReader(new FileReader(filePath));	
+			
+			try {
+				String line;
+				while ((line = buff.readLine()) != null) {
+					question += line;
+				}
+			} finally {
+				buff.close();
+			}
+		} catch (IOException ioe) {
+			System.out.println("Erreur --" + ioe.toString());
+		}
+		return question;
+	}
+
+}
--- src/main/java/main/MainWiki.java
+++ src/main/java/main/MainWiki.java
@@ -6,10 +6,8 @@ import gift.GiftReaderException;
 import gift.WikiReader;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.Reader;
 import java.io.StringReader;
-import java.net.URL;
 
 import questions.implementation.QuestionImpl;
 import questions.interfaces.Question;
@@ -24,35 +22,31 @@ import reponses.interfaces.AnswerBlock;
 
 public class MainWiki {
 
-	public static void main(String[] args) throws IOException {
-//		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-//		URL input = classLoader.getResource("main/TestQuizz.txt");
-//		GiftQuizContentHandler handler = new GiftQuizContentHandler();
-//        WikiReader quizReader = new WikiReader();
-//        quizReader.setQuizContentHandler(handler);
-//        quizReader.readFichier(input);
-        
-        
-		GiftQuestionService me = new GiftQuestionService();
-		
-		try {
-			Quiz testQuestion = me.getQuizFromWikiText("main/TestQuizz.txt");
-			
-			for (Question quest : testQuestion.getQuestionList()) {
-				System.out.println("\nNom : " + quest.getTextBlockList().get(0).getText());
-				System.out.println("Reponses : ");
-				for (AnswerBlock txt : quest.getAnswerBlockList()) {
-					for (Answer ans : txt.getAnswerList()) {
-						System.out.println("- "+ans.getTextValue());
-					}
-				}
-			}
-			//me.getQuizFromGiftText("{Question\n|type=\"()\"}\n+ The correct answer.\n- Distractor.\n- Distractor.\n- Distractor.");
-		} catch (GiftReaderException | IOException e) {
-			System.out.println("erreur");
-			// TODO Bloc catch généré automatiquement
-			e.printStackTrace();
-		}
+	public static void main(String[] args) {
+		GiftQuizContentHandler handler = new GiftQuizContentHandler();
+        WikiReader quizReader = new WikiReader();
+        quizReader.setQuizContentHandler(handler);
+        quizReader.readFichier("C:\\Users\\user\\git\\TSaaP-parser\\src\\main\\java\\main\\TestQuizz.txt");
+//		GiftQuestionService me = new GiftQuestionService();
+//		
+//		try {
+//			Quiz testQuestion = me.getQuizFromWikiText("C:\\Users\\user\\git\\TSaaP-parser\\src\\main\\java\\main\\TestQuizz.txt");
+//			
+//			for (Question quest : testQuestion.getQuestionList()) {
+//				System.out.println("Titre Question : " + quest.getTitle());
+//				System.out.println("Question : " + quest.getTextBlockList().get(0).getText());
+//				System.out.println("Reponses : ");
+//				for (AnswerBlock txt : quest.getAnswerBlockList()) {
+//					for (Answer ans : txt.getAnswerList()) {
+//						System.out.println("- "+ans.getTextValue());
+//					}
+//				}
+//			}
+//			//me.getQuizFromGiftText("{Question\n|type=\"()\"}\n+ The correct answer.\n- Distractor.\n- Distractor.\n- Distractor.");
+//		} catch (GiftReaderException | IOException e) {
+//			// TODO Bloc catch généré automatiquement
+//			e.printStackTrace();
+//		}
 	}
 
 }
