854fd37a2889df61febf81e07b89dd431a647ca9
Tartofraize <aymeric.davant@gmail.com>
2014-03-25 17:24:18 +0100

    #9 Ajout d'un test unitaire pour le package questions.

--- src/test/java/questions/implementation/QuestionImplTest.java
+++ src/test/java/questions/implementation/QuestionImplTest.java
@@ -1,54 +1,120 @@
 package questions.implementation;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import questions.interfaces.QuestionType;
+import questions.interfaces.TextBlock;
+import reponses.implementation.AnswerBlockImpl;
 
 public class QuestionImplTest {
 
+	private QuestionImpl question;
+
+	@Before
+	public void initialize() {
+		question = new QuestionImpl();
+	}
+
+	@After
+	public void clean() {
+		question = null;
+	}
+
 	@Test
 	public void testGetTitle() {
-		fail("Not yet implemented");
+		assertEquals(question.getTitle(), null);
+
+		question.setTitle("A title");
+		assertEquals(question.getTitle(), "A title");
 	}
 
 	@Test
 	public void testGetQuestionType() {
-		fail("Not yet implemented");
+		assertEquals(question.getQuestionType(), QuestionType.Undefined);
+
+		question.setQuestionType(QuestionType.ExclusiveChoice);
+		assertEquals(question.getQuestionType(), QuestionType.ExclusiveChoice);
 	}
 
 	@Test
 	public void testSetQuestionType() {
-		fail("Not yet implemented");
+		assertEquals(question.getQuestionType(), QuestionType.Undefined);
+
+		question.setQuestionType(QuestionType.ExclusiveChoice);
+		assertEquals(question.getQuestionType(), QuestionType.ExclusiveChoice);
+
+		assertNotEquals(question.getQuestionType(), QuestionType.Undefined);
+
+		// Added for code coverage
+		QuestionType qType = QuestionType.TrueFalse;
+		assertNotEquals(qType.getCode(), 0);
 	}
 
 	@Test
 	public void testSetTitle() {
-		fail("Not yet implemented");
+		assertEquals(question.getTitle(), null);
+
+		question.setTitle("A title");
+		assertEquals(question.getTitle(), "A title");
+
+		assertNotEquals(question.getTitle(), null);
 	}
 
 	@Test
 	public void testGetBlockList() {
-		fail("Not yet implemented");
+		assertTrue(question.getBlockList().isEmpty());
+
+		question.addAnswerBlock(new AnswerBlockImpl());
+		assertFalse(question.getBlockList().isEmpty());
 	}
 
 	@Test
 	public void testAddAnswerBlock() {
-		fail("Not yet implemented");
+		assertTrue(question.getAnswerBlockList().isEmpty());
+
+		question.addAnswerBlock(new AnswerBlockImpl());
+		assertFalse(question.getAnswerBlockList().isEmpty());
 	}
 
 	@Test
 	public void testAddTextBlock() {
-		fail("Not yet implemented");
+		assertTrue(question.getTextBlockList().isEmpty());
+
+		question.addTextBlock(new TextBlock() {
+			@Override
+			public String getText() {
+				return "A text";
+			}
+		});
+		assertFalse(question.getTextBlockList().isEmpty());
 	}
 
 	@Test
 	public void testGetAnswerBlockList() {
-		fail("Not yet implemented");
+		assertTrue(question.getAnswerBlockList().isEmpty());
+
+		question.addAnswerBlock(new AnswerBlockImpl());
+		assertFalse(question.getAnswerBlockList().isEmpty());
 	}
 
 	@Test
 	public void testGetTextBlockList() {
-		fail("Not yet implemented");
+		assertTrue(question.getTextBlockList().isEmpty());
+
+		question.addTextBlock(new TextBlock() {
+			@Override
+			public String getText() {
+				return "A text";
+			}
+		});
+		assertFalse(question.getTextBlockList().isEmpty());
 	}
 
 }
